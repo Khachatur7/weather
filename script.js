a67d9643104155091d1ed6ba5334e4925ced518c
Khachatur7 <152057828+Khachatur7@users.noreply.github.com>
2024-03-29 16:12:10 +0300

    Update script.js res

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 const api_key = "b9b90817d4a049c6ae7102853242703";
 const url = `http://api.weatherapi.com/v1/current.json`;
-//°C
-//°F
 let weather_content = document.getElementById("weather_content")
 let search_city = document.getElementById("search_city");
 let search_bttn = document.getElementById("search_bttn");
@@ -21,11 +19,10 @@ let weather_icons = [
 ]
 
 function fetchWeather() {
-    // weather_content.innerHTML = ""
     let resUrl = url + `?key=${api_key}&q=${search_city.value}`;
     fetch(resUrl)
         .then((res) => res.json())
-        .then((res) => showResult(res.current))
+        .then((res) => showResult(res))
         .catch((err) => {
             weather_content.innerHTML = '<h3>Not found<h3/>';
             console.log(err);
@@ -34,20 +31,20 @@ function fetchWeather() {
 
 function showResult(res) {
     console.log(res);
-    let temp = res.temp_c > 0 ? "+" + res.temp_c : res.temp_c
-    temp_num = temp > 0 ? `+ ${temp} <span class="cricle">°</span>C` : `${temp} <span class="cricle">°</span>C`;
-    wind = `${Math.floor(res.wind_kph / 3.6)} m/s, ${res.wind_dir}`
+  let data = res.current
+    let temp = data.temp_c > 0 ? "+" + data.temp_c : data.temp_c
+    wind = `${Math.floor(data.wind_kph / 3.6)} m/s, ${data.wind_dir}`
 
-    let child = `<div class="city" id="city"><span>Weather in ${search_city.value}</span></div>
+    let child = `<div class="city" id="city"><span>Weather in ${res.location.name}</span></div>
     <div class="weather">
       <div class="temp">
         <div class="temp_num" id="temp_num">${temp}<span class="cricle">°</span>C</div>
         <div class="icon" id="icon">
-          <img src="images/${weather_icons.includes(res.condition.text)?res.condition.text:res.condition.icon}.svg" alt="" />
+          <img src="images/${weather_icons.includes(data.condition.text)?data.condition.text:data.condition.icon}.svg" alt="" />
         </div>
         <div class="cond_feels" id="cond_feels">
-          <span>${res.condition.text}</span>
-          <span>Feels like: ${res.feelslike_c} <span class="cricle">°</span>C</span>
+          <span>${data.condition.text}</span>
+          <span>Feels like: ${data.feelslike_c} <span class="cricle">°</span>C</span>
         </div>
       </div>
       <div class="additionally">
@@ -55,13 +52,13 @@ function showResult(res) {
           <img src="images/wind.svg" alt="wind" /> <span>${wind}</span>
         </div>
         <div class="pressure" id="pressure">
-          <img src="images/pressure.svg" alt="pressure" /> <span>${res.pressure_mb}</span> mm Hg
+          <img src="images/pressure.svg" alt="pressure" /> <span>${data.pressure_mb}</span> mm Hg
         </div>
         <div class="humidity" id="humidity">
-          <img src="images/humidity.svg" alt="humidity" /> <span>${res.humidity}</span>%
+          <img src="images/humidity.svg" alt="humidity" /> <span>${data.humidity}</span>%
         </div>
         <div class="visibility" id="visibility">
-          <img src="images/visibility.svg" alt="visibility" /> <span>${res.vis_km}</span>km
+          <img src="images/visibility.svg" alt="visibility" /> <span>${data.vis_km}</span>km
         </div>
       </div>
     </div>`
